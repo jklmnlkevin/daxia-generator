b6e0c3e25c42a18f9d8a9826840b85964c0f7bbc
zhukewen_java <zhukewen_java@126.com>
2015-05-10 19:23:19 +0800

    save code

--- src/main/java/com/daxia/generator/util/k/TemplateUtils_K.java
+++ src/main/java/com/daxia/generator/util/k/TemplateUtils_K.java
@@ -1,9 +1,15 @@
 package com.daxia.generator.util.k;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -13,15 +19,24 @@ import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
 
 public class TemplateUtils_K {
+    private static String _projectPath;
+    private static String _basePackage;
 	public static void generate(GenerateType_K[] generateTypes, Map<String, Object> paramMap, String projectPath) throws Exception {
+	    _projectPath = projectPath;
 		for (GenerateType_K type : generateTypes) {
 	        generate(type, paramMap, projectPath);
         }
+		String Model = (String) paramMap.get("Model");
+        String model = (String) paramMap.get("model");
+		processBaseController(projectPath, _basePackage, Model, model);
 	}
 	
 	public static void generate(GenerateType_K generateType, Map<String, Object> paramMap, String projectPath) throws Exception {
 		String basePackage = (String) paramMap.get("basePackage");
 		basePackage = basePackage.replace(".", "/");
+		
+		_basePackage = basePackage;
+		
 		String Model = (String) paramMap.get("Model");
 		String model = (String) paramMap.get("model");
 		String templateName = generateType.name() + ".txt";
@@ -52,5 +67,42 @@ public class TemplateUtils_K {
         if (writer != null) {
         	writer.close();
         }
+        
+    }
+
+    private static void processBaseController(String projectPath, String basePackage, String Model, String model) throws Exception {
+        File baseController = new File(projectPath + "/src/main/java/com/daxia/core/web/controller/BaseController.java");
+        File mBaseController = new File(projectPath + "/src/main/java/" + basePackage + "/web/controller/m/MBaseController.java");
+        
+        String content = "    @Autowired\n    protected {Model}Service  {model}Service;\n";
+        content = content.replace("{Model}", Model);
+        content = content.replace("{model}", model);
+        
+        insertService(baseController, content);
+        insertService(mBaseController, content);
+    }
+    
+    private static void insertService(File file, String content) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        String line = null;
+        List<String> list = new ArrayList<String>();
+        while ((line = reader.readLine()) != null) {
+            if (line.contains(" class ") && line.contains("public")) {
+                list.add(line);
+                System.out.println(line);
+                list.add(content);
+            } else {
+                list.add(line);
+            }
+        }
+        reader.close();
+        
+        FileWriter writer = new FileWriter(file);
+        for (String str : list) {
+            writer.write(str);
+            writer.write("\n");
+        }
+        writer.close();
+         
     }
 }
